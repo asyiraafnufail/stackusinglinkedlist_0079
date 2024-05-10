3b0074631c38c05a13276a5df5c13451e85cd5c0
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-10 13:49:04 +0700

    menuliskan switch

--- stackusinglinkedlist_0079.cpp
+++ stackusinglinkedlist_0079.cpp
@@ -70,5 +70,39 @@ int main(){
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice){
+            case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+            
+            case 2:
+            if (!stack.isEmpty()){
+                stack.pop();
+            }
+            else{
+                cout << "Stack is empty. cannot pop." << endl;
+            }
+            break;
+
+            case 3:
+            if (!stack.isEmpty()){
+                stack.peek();
+            }
+            else{
+                cout << "Stack is empty. No top value" << endl;
+            }
+            break;
+
+            case 4:
+            cout << "Exiting program." << endl;
+            break;
+
+            default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
     }
 }
