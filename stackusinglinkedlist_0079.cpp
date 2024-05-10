2701ca6a0d0aaf28204aa47fa00515148e39d2ff
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-10 14:00:03 +0700

    memperbaiki kurang spasi pada cout peek

--- stackusinglinkedlist_0079.cpp
+++ stackusinglinkedlist_0079.cpp
@@ -66,7 +66,7 @@ int main(){
     while (choice != 5){
         cout << "1. Push\n";
         cout << "2. Pop\n";
-        cout << "3.Peek\n";
+        cout << "3. Peek\n";
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
