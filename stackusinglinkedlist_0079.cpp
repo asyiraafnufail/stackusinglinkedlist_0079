e75bc3c26e8571c123902b8a5169fb21c88c9688
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-10 13:31:10 +0700

    menuliskan int push

--- stackusinglinkedlist_0079.cpp
+++ stackusinglinkedlist_0079.cpp
@@ -20,5 +20,11 @@ class Stack{
         top = NULL;
     }
 
-
+    int push(int value){
+        Node* newNode = new Node();
+        newNode -> data = value;
+        newNode -> next = top;
+        top = newNode;
+        
+    }
 }
