963db05cd8559e1c10bc3e5aa19c563d4feb3520
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-10 13:31:49 +0700

    menuliskan cout value

--- stackusinglinkedlist_0079.cpp
+++ stackusinglinkedlist_0079.cpp
@@ -25,6 +25,7 @@ class Stack{
         newNode -> data = value;
         newNode -> next = top;
         top = newNode;
-        
+        cout << "Push value: " << value << endl;
+        return value;
     }
 }
